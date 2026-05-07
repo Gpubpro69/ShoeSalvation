db06c8a857646186cfb576cef0380da3cc7830ec
Gpubpro69 <gpubpro69@gmail.com>
2026-05-07 21:04:49 +0545

    feat: add AppDbContext with all entity DbSets

---
+++ src/ShoeSalvation.Repository/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoeSalvation.Domain.Entities;
+namespace ShoeSalvation.Repository.Data;
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<PriceHistory> PriceHistories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<ProductVariant> ProductVariants { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<Return> Returns { get; set; }
+    public DbSet<StockAdjustment> StockAdjustments { get; set; }
+    public DbSet<SubCategory> SubCategories { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Wishlist> Wishlists { get; set; }
+    public DbSet<WishlistItem> WishlistItems { get; set; }
+
+}
+
+
+
